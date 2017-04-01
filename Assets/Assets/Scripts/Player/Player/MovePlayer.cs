5b5585c4d140728b1398bc3be13da843c960451f
minydoc <minydoc@gmail.com>
2017-04-01 13:51:14 +0200

    Gros commit

--- Assets/Assets/Scripts/Player/Player/MovePlayer.cs
+++ Assets/Assets/Scripts/Player/Player/MovePlayer.cs
@@ -10,11 +10,6 @@ public class MovePlayer : MonoBehaviour {
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	public Animator anim;
-	public bool mainpleine;
-	public bool Pistol;
-	public bool Riffle;
-
-
 
 	private Vector3 moveDirection = Vector3.zero;
 	private float InputH;
@@ -33,11 +28,6 @@ public class MovePlayer : MonoBehaviour {
 		anim = GetComponent<Animator> ();
 		run = false;
 		view = GetComponent<PhotonView> ();
-		mainpleine = false;
-		Pistolet = GameObject.FindGameObjectWithTag ("Pistolet");
-		Pistolet.active = false;
-		Pistol = false;
-		Riffle = false;
 		aim = false;
 	}
 	
@@ -45,45 +35,35 @@ public class MovePlayer : MonoBehaviour {
 	void Update () {
 		CharacterController controller = GetComponent<CharacterController> ();
 		if (view.isMine) {
-			if (controller.isGrounded) {
-					InputH = Input.GetAxis ("Horizontal");
-					InputV = Input.GetAxis ("Vertical");			
+				if (controller.isGrounded) {
+						InputH = Input.GetAxis ("Horizontal");
+						InputV = Input.GetAxis ("Vertical");			
+				}
+
+				float y = sensi_camera_X * Input.GetAxis ("Mouse X");
+				float x = transform.rotation.x;
+				float z = transform.rotation.z;
+				transform.Rotate (x, y, z);
+
+				// appuyer sur shift pour courrir
+				if ((Input.GetKey (KeyCode.LeftShift)) || (Input.GetKey (KeyCode.RightShift))) {
+					run = true;
+				} else {
+					run = false;
+				}
+				
+				// Clicker droit pour viser/Zoomer
+				if (Input.GetMouseButtonDown (1)) {
+					aim = true;
+					anim.SetBool ("Aim", aim);
+				}
+				if (Input.GetMouseButtonUp (1)) {
+					aim = false;
+					anim.SetBool ("Aim", aim);
+				}
 			}
 
 
-			moveDirection.y -= gravity * Time.deltaTime;
-			controller.Move (moveDirection * Time.deltaTime);
-			float y = sensi_camera_X * Input.GetAxis ("Mouse X");
-			float x = transform.rotation.x;
-			float z = transform.rotation.z;
-			transform.Rotate (x, y, z);
-
-
-			if ((Input.GetKey (KeyCode.Space))) {
-				mainpleine = !mainpleine;
-				anim.SetBool ("mainpleine", mainpleine);
-				Pistolet.active = !Pistolet.active;
-				Pistol = !Pistol;
-				anim.SetBool ("Pistol", Pistol);
-			}
-
-			if ((Input.GetKey (KeyCode.LeftShift)) || (Input.GetKey (KeyCode.RightShift))) {
-				run = true;
-			} else {
-				run = false;
-			}
-
-			if (Input.GetMouseButtonDown (1)) {
-				aim = true;
-				anim.SetBool ("Aim", aim);
-			}
-			if (Input.GetMouseButtonUp (1)) {
-				aim = false;
-				anim.SetBool ("Aim", aim);
-			}
-
-
-			}
 		anim.SetFloat ("InputH", InputH);
 		anim.SetFloat ("InputV", InputV);
 		anim.SetBool ("Run", run);
